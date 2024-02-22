c5095f8cd6f4966e22d25b7ea6f31bb2f0a26bb1
BillCipherx <160838436+BillCipherx@users.noreply.github.com>
2024-02-22 15:34:40 +0630

    Add files via upload

---
+++ Allcourses.js
@@ -0,0 +1,9 @@
+function get(obj) {
+    return document.getElementById(obj);
+}
+function switchtree(){
+    get('Box').style.display="flex";
+}
+function switchfour(){
+    get('Box').style.display="none";
+}
---
+++ Policies.js
@@ -0,0 +1,183 @@
+function get(obj) {
+    return document.getElementById(obj);
+}
+function switchone(){
+    get('tab1').style.background="white";
+    get('Panel1').style.display="block";
+    get('tab2').style.background="rgb(30, 30, 30)";
+    get('Panel2').style.display="none";
+    get('tab3').style.background="rgb(30, 30, 30)";
+    get('Panel3').style.display="none";
+    get('tab4').style.background="rgb(30, 30, 30)";
+    get('Panel4').style.display="none"; 
+    get('tab5').style.background="rgb(30, 30, 30)";
+    get('Panel5').style.display="none"; 
+    get('tab6').style.background="rgb(30, 30, 30)";
+    get('Panel6').style.display="none"; 
+    get('tab7').style.background="rgb(30, 30, 30)";
+    get('Panel7').style.display="none"; 
+    get('tab8').style.background="rgb(30, 30, 30)";
+    get('Panel8').style.display="none"; 
+    get('tab9').style.background="rgb(30, 30, 30)";
+    get('Panel9').style.display="none"; 
+}
+function switchtwo(){
+    get('tab2').style.background="white";
+    get('Panel2').style.display="block";
+    get('tab1').style.background="rgb(30, 30, 30)";
+    get('Panel1').style.display="none";
+    get('tab3').style.background="rgb(30, 30, 30)";
+    get('Panel3').style.display="none"; 
+    get('tab4').style.background="rgb(30, 30, 30)";
+    get('Panel4').style.display="none"; 
+    get('tab5').style.background="rgb(30, 30, 30)";
+    get('Panel5').style.display="none"; 
+    get('tab6').style.background="rgb(30, 30, 30)";
+    get('Panel6').style.display="none"; 
+    get('tab7').style.background="rgb(30, 30, 30)";
+    get('Panel7').style.display="none"; 
+    get('tab8').style.background="rgb(30, 30, 30)";
+    get('Panel8').style.display="none"; 
+    get('tab9').style.background="rgb(30, 30, 30)";
+    get('Panel9').style.display="none";
+}
+function switchtree(){
+    get('tab3').style.background="white";
+    get('Panel3').style.display="block";
+    get('tab2').style.background="rgb(30, 30, 30)";
+    get('Panel2').style.display="none";
+    get('tab1').style.background="rgb(30, 30, 30)";
+    get('Panel1').style.display="none"; 
+    get('tab4').style.background="rgb(30, 30, 30)";
+    get('Panel4').style.display="none"; 
+    get('tab5').style.background="rgb(30, 30, 30)";
+    get('Panel5').style.display="none"; 
+    get('tab6').style.background="rgb(30, 30, 30)";
+    get('Panel6').style.display="none"; 
+    get('tab7').style.background="rgb(30, 30, 30)";
+    get('Panel7').style.display="none"; 
+    get('tab8').style.background="rgb(30, 30, 30)";
+    get('Panel8').style.display="none"; 
+    get('tab9').style.background="rgb(30, 30, 30)";
+    get('Panel9').style.display="none";
+}
+function switchfour(){
+    get('tab4').style.background="white";
+    get('Panel4').style.display="block";
+    get('tab2').style.background="rgb(30, 30, 30)";
+    get('Panel2').style.display="none";
+    get('tab3').style.background="rgb(30, 30, 30)";
+    get('Panel3').style.display="none";
+    get('tab1').style.background="rgb(30, 30, 30)";
+    get('Panel1').style.display="none"; 
+    get('tab5').style.background="rgb(30, 30, 30)";
+    get('Panel5').style.display="none"; 
+    get('tab6').style.background="rgb(30, 30, 30)";
+    get('Panel6').style.display="none"; 
+    get('tab7').style.background="rgb(30, 30, 30)";
+    get('Panel7').style.display="none"; 
+    get('tab8').style.background="rgb(30, 30, 30)";
+    get('Panel8').style.display="none"; 
+    get('tab9').style.background="rgb(30, 30, 30)";
+    get('Panel9').style.display="none";  
+}
+function switchfive(){
+    get('tab5').style.background="white";
+    get('Panel5').style.display="block";
+    get('tab2').style.background="rgb(30, 30, 30)";
+    get('Panel2').style.display="none";
+    get('tab3').style.background="rgb(30, 30, 30)";
+    get('Panel3').style.display="none";
+    get('tab4').style.background="rgb(30, 30, 30)";
+    get('Panel4').style.display="none"; 
+    get('tab1').style.background="rgb(30, 30, 30)";
+    get('Panel1').style.display="none"; 
+    get('tab6').style.background="rgb(30, 30, 30)";
+    get('Panel6').style.display="none"; 
+    get('tab7').style.background="rgb(30, 30, 30)";
+    get('Panel7').style.display="none"; 
+    get('tab8').style.background="rgb(30, 30, 30)";
+    get('Panel8').style.display="none"; 
+    get('tab9').style.background="rgb(30, 30, 30)";
+    get('Panel9').style.display="none"; 
+}
+function switchsix(){
+    get('tab6').style.background="white";
+    get('Panel6').style.display="block";
+    get('tab2').style.background="rgb(30, 30, 30)";
+    get('Panel2').style.display="none";
+    get('tab3').style.background="rgb(30, 30, 30)";
+    get('Panel3').style.display="none";
+    get('tab4').style.background="rgb(30, 30, 30)";
+    get('Panel4').style.display="none"; 
+    get('tab5').style.background="rgb(30, 30, 30)";
+    get('Panel5').style.display="none"; 
+    get('tab1').style.background="rgb(30, 30, 30)";
+    get('Panel1').style.display="none"; 
+    get('tab7').style.background="rgb(30, 30, 30)";
+    get('Panel7').style.display="none"; 
+    get('tab8').style.background="rgb(30, 30, 30)";
+    get('Panel8').style.display="none"; 
+    get('tab9').style.background="rgb(30, 30, 30)";
+    get('Panel9').style.display="none"; 
+}
+function switchsenven(){
+    get('tab7').style.background="white";
+    get('Panel7').style.display="block";
+    get('tab2').style.background="rgb(30, 30, 30)";
+    get('Panel2').style.display="none";
+    get('tab3').style.background="rgb(30, 30, 30)";
+    get('Panel3').style.display="none";
+    get('tab4').style.background="rgb(30, 30, 30)";
+    get('Panel4').style.display="none"; 
+    get('tab5').style.background="rgb(30, 30, 30)";
+    get('Panel5').style.display="none"; 
+    get('tab6').style.background="rgb(30, 30, 30)";
+    get('Panel6').style.display="none"; 
+    get('tab1').style.background="rgb(30, 30, 30)";
+    get('Panel1').style.display="none"; 
+    get('tab8').style.background="rgb(30, 30, 30)";
+    get('Panel8').style.display="none"; 
+    get('tab9').style.background="rgb(30, 30, 30)";
+    get('Panel9').style.display="none"; 
+}
+function switcheight(){
+    get('tab8').style.background="white";
+    get('Panel8').style.display="block";
+    get('tab2').style.background="rgb(30, 30, 30)";
+    get('Panel2').style.display="none";
+    get('tab3').style.background="rgb(30, 30, 30)";
+    get('Panel3').style.display="none";
+    get('tab4').style.background="rgb(30, 30, 30)";
+    get('Panel4').style.display="none"; 
+    get('tab5').style.background="rgb(30, 30, 30)";
+    get('Panel5').style.display="none"; 
+    get('tab6').style.background="rgb(30, 30, 30)";
+    get('Panel6').style.display="none"; 
+    get('tab7').style.background="rgb(30, 30, 30)";
+    get('Panel7').style.display="none"; 
+    get('tab1').style.background="rgb(30, 30, 30)";
+    get('Panel1').style.display="none"; 
+    get('tab9').style.background="rgb(30, 30, 30)";
+    get('Panel9').style.display="none"; 
+}
+function switchnine(){
+    get('tab9').style.background="white";
+    get('Panel9').style.display="block";
+    get('tab2').style.background="rgb(30, 30, 30)";
+    get('Panel2').style.display="none";
+    get('tab3').style.background="rgb(30, 30, 30)";
+    get('Panel3').style.display="none";
+    get('tab4').style.background="rgb(30, 30, 30)";
+    get('Panel4').style.display="none"; 
+    get('tab5').style.background="rgb(30, 30, 30)";
+    get('Panel5').style.display="none"; 
+    get('tab6').style.background="rgb(30, 30, 30)";
+    get('Panel6').style.display="none"; 
+    get('tab7').style.background="rgb(30, 30, 30)";
+    get('Panel7').style.display="none"; 
+    get('tab8').style.background="rgb(30, 30, 30)";
+    get('Panel8').style.display="none"; 
+    get('tab1').style.background="rgb(30, 30, 30)";
+    get('Panel1').style.display="none"; 
+}
